55eaca600524b16d9c692e299586031fdf1168c0
sabarish48 <sabarishsankar89@yahoo.co.in>
2012-08-25 00:56:00 +0530

    initial set of rails code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypals::Application.config.secret_token = '946525334c9fb68c0f9a55d138e21f12e17d15327cda76b874e78cebe12da51052cbc2d63f4d88c530ba86f3628f8d568fb1cd08e3224a8e7f2b595b5e64b714'
